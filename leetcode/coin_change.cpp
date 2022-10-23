a7be6c3aa3227dacb0026971c1a380cafe6f5aef
samyhaff <haffoudhisamy@gmail.com>
2022-10-23 10:32:10 +0200

    new problem

---
+++ leetcode/coin_change.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int value: coins) {
+                if (value <= i && dp[i - value] != INT_MAX && dp[i - value] + 1 < dp[i])
+                    dp[i] = 1 + dp[i - value];
+            }
+        }
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
+    }
+};
