cad58f68776e08a4b4035f405cbdf7dbafbecc79
samyhaff <haffoudhisamy@gmail.com>
2022-11-16 21:24:37 +0100

    new problem

---
+++ leetcode/two_sums_ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i = 0, j = numbers.size() - 1;
+        while (true) {
+            if (numbers[i] + numbers[j] == target)
+                return {i + 1, j + 1};
+            else if (numbers[i] + numbers[j] > target)
+                j--;
+            else if (numbers[i] + numbers[j] < target)
+                i++;
+        }
+    }
+};
